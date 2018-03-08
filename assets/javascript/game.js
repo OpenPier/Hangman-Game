2fc7b309a42053096592e29d8cfa1ce34ffd5d30
OpenPier <35052947+OpenPier@users.noreply.github.com>
2018-03-07 23:51:18 -0600

    1.0.4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,7 @@ var lossCount =0;
 var guessesLeft = 9;
 
 //Functions of the game (Reusable blocks of cod that I will call upon when needed)
-funtion startGame () {
+function startGame () {
     selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
     lettersinWord = selectedWord.split("");
     numBlanks = lettersinWord.length;
@@ -33,10 +33,10 @@ funtion startGame () {
     }
 
     //chanage HTML to reflect round conditions
-    document.getElementById["wordToGuess"].innerHTML = blanksAndSuccesses.join["  "];
-    document.getElementById["numGuesses"].innerHTML = guessesLeft;
-    document.getElementById["winCounter"].innerHTML = winCount;
-    document.getElementById["lossCounter"].innerHTML = lossCount;
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join("  ");
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+    document.getElementById("lossCounter").innerHTML = lossCount;
 
 
     //Testing /Debuggin
@@ -45,16 +45,98 @@ funtion startGame () {
     console.log [numBlanks];
     console.log [blanksAndSuccesses];
 }
+function checkletters(letter) {
+    //check if letter exists
+    var isLetterInWord = false;
 
+    for (var i=0; i<numBlanks; i++ ){
+        if(selectedWord[i] == letter) {
+            lettersinWord = true;
+                alert("Letter Found")
+        }
+    }
+    //Check where in the Letter
+    if(isLetterInWord) {
+        for (var i=0; i<numBlanks; i++){
+            if(selectedWord[i] == letter) {
+                blanksAndSuccesses[i] = letter;
+            }
+        }
+    }
+    else {
+        wrongLetters.push(letter);
+        guessesLeft--;
+       }
+}
+
+//Letter wasn't found
+
+
+   console.log(blanksAndSuccesses);
+
+function roundComplete() {
+    console.log["Win Count: " + winCount +" | Loss Count: " + lossCount + "| Guesses left" + numGuesses];
+
+    //Update the HTML to reflect the most recent count stats
+    document.getElementById["numGuesses"].innerHTML = guessesLeft;
+    document.getElementById["wordToGuess"].innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById["wrongGuesses"].innerHTML = wrongLetters.join(" ");
+
+
+    //Check if user won
+    if(lettersinWord.toString()== blanksAndSuccesses.toString()){
+        winCount++;
+        alert("You Won!");
+
+        //Update the win counter to the HTML
+        document.getElementById("winCounter").innerHTML = winCount;
+        
+        startGame();
+    }
+//Check if user lost
+    else if (numGuesses == 0) {
+        lossCount++;
+        alert("You lost!");
+
+        //Update the HTML
+        document.getElementById("lostCounter").innerHTML * lossCount;
+
+        startGame();
+    }
+}
+    
 //Main Process
 
 //Initiates the code
-startGame[];
+startGame();
 
 //register key clicks
 
 document.onkeyup=function(event) {
     var letterGuessed = String.fromCharCode(event.keyCode).toLocaleLowerCase();
-    alert
+    checkletters(letterGuessed);
+    roundComplete;
+    //testing/debuggin
+    console.log (letterGuessed);
     
 }
+
+
+
+
+
+
+
+
+https://itunes.apple.com/us/album/2am/1278503345?i=1278503350
+
+//Regagee music playing 
+var music = document.createElement("audio");
+music.setAttribute("src", "pathToYourMp3File");
+music.play();
+
+//loop if you want your song to keep playing
+music.addEventListener('ended', function () {
+    this.currentTime = 0;
+    this.play();
+}, false);
